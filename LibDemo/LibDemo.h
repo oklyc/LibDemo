e3f894c8aca335521764c57df17a306d1d6f821b
oklyc <liyc@liycdeMac-mini.local>
2015-11-04 10:30:50 +0800

    测试lib示例

---
+++ LibDemo/LibDemo.h
@@ -0,0 +1,15 @@
+//
+//  LibDemo.h
+//  LibDemo
+//
+//  Created by liyc on 15/11/4.
+//  Copyright © 2015年 liyc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LibDemo : NSObject
+
++ (void)logSomething;
+
+@end
---
+++ LibDemo/LibDemo.m
@@ -0,0 +1,18 @@
+//
+//  LibDemo.m
+//  LibDemo
+//
+//  Created by liyc on 15/11/4.
+//  Copyright © 2015年 liyc. All rights reserved.
+//
+
+#import "LibDemo.h"
+
+@implementation LibDemo
+
++ (void)logSomething
+{
+    NSLog(@"hello lib demo");
+}
+
+@end
